beae7cc08e309ee02a33f527acbc87d3b5e5aad6
Ths2-9Y-LqJt6 <Ths2-9Y-LqJt6@plip.com>
2020-01-07 15:03:37 -0800

    revert changed call to updateTaxAndTotal()

--- js/invoice.js
+++ js/invoice.js
@@ -22,7 +22,7 @@ $(document).ready(function () {
     showDeleteButton(this);
 
     $('#tax').on('blur', function () {
-        updateTaxAndTotal();
+        updateTaxAndTotal(this);
     });
 });
 
